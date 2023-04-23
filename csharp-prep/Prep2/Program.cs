ab40de6aba6a86c6492f9da46bb52af170d780ee
taylynnharman <dyer.taylynn@gmail.com>
2023-04-22 18:53:46 -0600

    Grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string user = Console.ReadLine();
+        int percent = int.Parse(user);
+
+        string grade = "";
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
