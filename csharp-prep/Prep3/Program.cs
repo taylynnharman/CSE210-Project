233f50739f70afda0d3d99e654e66d77cd27646c
taylynnharman <dyer.taylynn@gmail.com>
2023-04-26 16:10:59 -0600

    added loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,39 +5,31 @@ class Program
     static void Main(string[] args)
     {
 //Ask the user to enter a magic number
-        Console.WriteLine("User 1, please enter the magic number: ");
+        Console.Write("User 1, please enter the magic number: ");
         int number = Convert.ToInt32(Console.ReadLine());
         
+        int guess = 10032450;
+
+//Add a loop that keeps looping as long as the guess does not match the magic number.
+        while (guess!= number) {
+
 //Ask the user for a guess.
-        Console.WriteLine("User 2, what is your guess: ");
-        int guess = Convert.ToInt32(Console.ReadLine());
-
+                Console.Write("User 2, what is your guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
 // Higher message
-            if (guess < number)
-            {Console.Write("Higher");}
+                if (guess < number)
+                {Console.WriteLine("Higher");}
 //Lower message
-            else if (guess > number) 
-            {Console.Write("Lower");}
+                else if (guess > number) 
+                {Console.WriteLine("Lower");}
 //Correct message
-            else {
-            Console.Write("Congrats! You guessed the magic number!");
-            }
-
+                else 
+                {Console.WriteLine("Congrats! You guessed the magic number!");}
+        }
     }
 }
 
-
-
-  
-//Add a loop that keeps looping as long as the guess does not match the magic number.
-
-//At this point, the user should be able to keep playing until they get the correct answer.
-
-//The following shows the expected output at this point:
-
 //Instead of having the user supply the magic number, generate a random number from 1 to 100.
-
-
 
 //Stretch Challenge
 //Keep track of how many guesses the user has made and inform them of it at the end of the game.
