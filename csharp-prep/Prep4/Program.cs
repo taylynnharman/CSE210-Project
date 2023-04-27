f73e1b751d36c435f552946776656e7fb5d7d1b2
taylynnharman <dyer.taylynn@gmail.com>
2023-04-26 23:47:33 -0600

    Basics Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+//Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+        List<int> numbers = new List<int>();
+        int user_num = 209349;
+
+        while (user_num != 0) {
+        Console.WriteLine("Please enter a series of numbers; 1 at a time. Enter 0 when finished: ");
+        Console.WriteLine("Enter number: ");
+            user_num = Convert.ToInt32(Console.ReadLine());
+        
+        }
     }
 }
+
+//Core Requirements
+//Compute the sum, or total, of the numbers in the list.
+
+//Compute the average of the numbers in the list.
+
+//Find the maximum, or largest, number in the list.
