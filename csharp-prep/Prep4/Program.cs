db44411b87e32704e4868ceb0fef7f575afc6441
taylynnharman <dyer.taylynn@gmail.com>
2023-04-27 00:11:35 -0600

    core requirements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,22 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-//Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
         List<int> numbers = new List<int>();
         int user_num = 209349;
-
+        int sum = 0;
+        int max = 0;
+        int count = -1;
+//Stop when they enter 0
         while (user_num != 0) {
+
+////Ask the user for a series of numbers
         Console.WriteLine("Please enter a series of numbers; 1 at a time. Enter 0 when finished: ");
         Console.WriteLine("Enter number: ");
             user_num = Convert.ToInt32(Console.ReadLine());
-        
-        }
-    }
-}
 
-//Core Requirements
+//append each one to a list
+        numbers.Add(user_num);
 //Compute the sum, or total, of the numbers in the list.
 
-//Compute the average of the numbers in the list.
-
+        sum = sum + user_num; 
 //Find the maximum, or largest, number in the list.
+        if (user_num > max) {
+            max = user_num;
+        }
+//Compute the average of the numbers in the list.
+        count = count+1;
+        }
+    float average = sum/count; 
+
+Console.WriteLine($"List{numbers}");
+Console.WriteLine($"Sum:{sum}");
+Console.WriteLine($"Max:{max}");
+Console.WriteLine($"Average:{average}");
+Console.WriteLine($"Count{count}");
+
+    }
+}
