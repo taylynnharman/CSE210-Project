a008892544d9c63ca0d39f766898e1f3adf26e04
taylynnharman <dyer.taylynn@gmail.com>
2023-05-06 23:31:23 -0600

    Develop prompts, attempt to find random

--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -1,3 +1,14 @@
 public class Prompt {
-    
+    List<string>prompts = new List<string> {
+        "Who was the most interesting person I interacted with today?",
+"What was the best part of my day?",
+"How did I see the hand of the Lord in my life today?",
+"What was the strongest emotion I felt today?",
+"If I had one thing I could do over today, what would it be?",
+"What lesson did you learn today?",
+"How did you improve today?", 
+"Who do you feel prompted to reach out to today?"};
+
+int index = random.Next(list.Count);
+Console.WriteLine(list[index]);
 }
