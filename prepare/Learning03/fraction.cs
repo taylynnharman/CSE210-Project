5d9e61dd4d53673db4fb56eb40f357ea5aaf65bc
taylynnharman <dyer.taylynn@gmail.com>
2023-05-17 21:37:01 -0600

    constructors

--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -1,8 +1,26 @@
 using System;
 
 public class Fraction {
+    //attributes for top and bottom numbers
     private int _top;
     private int _bottom;
     
+    //constructor with no parameters
     public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int number)
+    {
+        //initialize bottom to 1
+        _top = number;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+
 }
