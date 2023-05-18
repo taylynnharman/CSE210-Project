e488fc16acaee28ae76025a6a92b2ee8e5d1cc63
taylynnharman <dyer.taylynn@gmail.com>
2023-05-17 22:37:42 -0600

    part 6

--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -11,16 +11,25 @@ public class Fraction {
         _top = 1;
         _bottom = 1;
     }
+    //constructor that will make numerator a whole number
     public Fraction(int number)
     {
         //initialize bottom to 1
         _top = number;
         _bottom = 1;
     }
+    //constructor for a top and bottom ?
     public Fraction(int top, int bottom){
         _top = top;
         _bottom = bottom;
     }
+    public string GetFractionString(){
+        string fractionString = $"{_top}/{_bottom}";
+        return fractionString;
+    }
 
+    public double GetDecimalValue(){
+        return (double)_top / (double)_bottom;
+    }
 
 }
