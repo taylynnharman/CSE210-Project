c258913b9a85c29f415e7a772c5f967ed532b101
taylynnharman <dyer.taylynn@gmail.com>
2023-07-13 13:00:24 -0600

    Changed duration to be in seconds

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -12,29 +12,56 @@ class Program
 class Lectures : Events {
     private string _speaker;
     private int _capacity;
+    public string Lecture(string title, string description, DateTime date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+        public override string GetFullDetails()
+    {
+        return $"{StandardDetails()}\nEvent Type: Lecture\nSpeaker: {speaker}\nCapacity: {capacity}";
+    }
+
+    public override string ShortDescription()
+    {
+        return $"Event Type: Lecture\nTitle: {title}\nDate: {date}";
+    }
 }
+
 //Receptions, which require people to RSVP, or register, beforehand.
 class Receptions : Events {
     private string _rsvpEmail;
 }
 //Outdoor gatherings, which do not have a limit on attendees, but need to track the weather forecast.
-class Outdoor_Gatherings : Events {
-    private string _weatherForcast;
+class OutdoorGatherings : Events {
+    private string _weatherForecast;
 //Details: For outdoor gatherings, this includes a statement of the weather.
-    public string WeatherStatement()
+    public OutdoorGatherings(string title, string description, DateTime date, string time, Address address, string weatherForecast)
+    : base(title, description, date, time, address)
+{
+    _weatherForecast = weatherForecast;
+}
+
+    public override string FullDetails()
     {
-        return $"Weather: {WeatherForcast}"
-    } 
+        return $"{StandardDetails()}\nEvent Type: Outdoor Gathering\nWeather Forecast: {weatherForecast}";
+    }
+
+    public override string ShortDescription()
+    {
+        return $"Event Type: Outdoor Gathering\nTitle: {title}\nDate: {date}";
+    }
 }
 //Regardless of the type, all events need to have an Event Title, Description, Date, Time, and Address.
 class Events {
     private string _title;
     private string _description;
     private DateTime _date;
-    private DateTime _time;
+    private string _time;
 //Use an address class for the addresses.
     private Address _address;
-    public Events (string title, string description, DateTime date, DateTime time, Address address)
+    public Events (string title, string description, DateTime date, string time, Address address)
     {
         _title = title;
        _description = description;
@@ -43,18 +70,13 @@ class Events {
        _address = address;
     }
 //Standard details - Lists the title, description, date, time, and address.
-    public StandardDetails()
+    public string StandardDetails()
     {
-        return $"Title: {Title}\nDescription: {Description}\nDate: {Date}\nTime: {Time}\nAddress: {Address}";
+        return $"Title: {Events.title}\nDescription: {description}\nDate: {date}\nTime: {time}\nAddress: {address}";
     }
 //Full details - Lists all of the above, plus type of event and information specific to that event type. For lectures, this includes the speaker name and capacity. For receptions this includes an email for RSVP. For outdoor gatherings, this includes a statement of the weather.
-    public FullDetails()
-    {
-        return $"Title: {Title} Description: {Description} Date: {Date} Time: {Time}Address: {Address}";
-    }
+    public abstract string FullDetails();
+    
 //Short description - Lists the type of event, title, and the date.
-    public ShortDescription()
-    {
-        return $" Title: {_title}, Date: {_date}";
-    }
+    public abstract string ShortDescription();
 }
